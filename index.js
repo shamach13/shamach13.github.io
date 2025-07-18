7274e683da3e04b6b881b4b595086874f5c14c8f
shamach13 <116988214+shamach13@users.noreply.github.com>
2025-07-18 20:26:30 +0530

    Add files via upload
    Initial

---
+++ index.js
@@ -0,0 +1,21 @@
+function post(page) {
+    switch (page) {
+      case 1:
+        //about me
+        window.location.href = "about_me.html";
+        break;
+      case 2:
+        //my qualification
+        window.location.href = "my_qual.html";
+        break;
+      case 3:
+        //my resume
+          window.location.href = "my_resume.html";
+        break;
+      case 4:
+        //contact
+          window.location.href = "my_contact.html";
+        break;
+    }
+  }
+  
